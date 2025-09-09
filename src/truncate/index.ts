76057a93ec3490d7c9967ee55f93ddec852bf0ef
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-09 10:53:49 +0330

    [FIX] id not reset on truncate

--- src/truncate/index.ts
+++ src/truncate/index.ts
@@ -9,25 +9,24 @@ import { validateEnv } from "@/utils/env.utils";
 async function main(): Promise<void> {
   validateEnv();
 
-  const databaseService = new DatabaseService(false);
+  console.log("Starting drop and sync...");
+
+  const databaseService = new DatabaseService({
+    synchronize: true,
+    dropSchema: true,
+  });
+
   const isDatabaseInitialized = await databaseService.init();
 
   if (!isDatabaseInitialized) {
     return;
   }
 
-  await dropAndSync(databaseService);
+  console.log("Drop and sync finished successfully.");
+
   await wipeFileStorage();
 
   await databaseService.dataSource.destroy();
-}
-
-async function dropAndSync(databaseService: DatabaseService): Promise<void> {
-  console.log("Starting drop and sync...");
-
-  await databaseService.dataSource.synchronize(true);
-
-  console.log("Drop and sync finished successfully.");
 }
 
 async function wipeFileStorage(): Promise<void> {
