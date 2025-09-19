4f52235d80ca08e42072eb857fe44bb43e9b4ab6
ZahraZare <zahrazare3099@gmail.com>
2025-09-19 13:51:08 +0330

    Update Ingredient schema

--- src/validation/schemas/ingredient.schema.ts
+++ src/validation/schemas/ingredient.schema.ts
@@ -2,8 +2,4 @@ import { z } from "zod";
 
 export const IngredientSchema = z.object({
   title: z.string().min(1),
-  amount: z
-    .number("Ingredient's amount must be a number.")
-    .positive("Ingredient's amount must be a positive number."),
-  unit: z.string(),
 });
